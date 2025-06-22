e199cdc0fd78eba97ba12b639f34a95cc5c16122
kaut0 <zilullah792@gmail.com>
2025-06-22 10:59:06 +0800

    adding overflow

--- app/src/main/java/com/example/scrollableapp/MainActivity.kt
+++ app/src/main/java/com/example/scrollableapp/MainActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.scrollableapp.affirmation.Affirmation
@@ -90,6 +91,8 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier){
             Text(
                 text = LocalContext.current.getString(affirmation.stringResourceId),
                 modifier = modifier.padding(16.dp),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.headlineSmall
             )
         }
